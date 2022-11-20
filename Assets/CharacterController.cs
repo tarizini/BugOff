e5f5ba28bba5eab3f67c4ed350ab9bdbd8a86258
tarizini <113103360+tarizini@users.noreply.github.com>
2022-11-20 01:02:05 +0000

    Added Ground Layer

--- Assets/CharacterController.cs
+++ Assets/CharacterController.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
+    public float superSpeed;
+    public float normalSpeed = 10.0f;
+    public float sprintSpeed = 20.0f;
+
     float maxSpeed = 1.0f;
     float rotation = 0.0f;
     float camRotation = 0.0f;
     GameObject cam;
     Rigidbody myRigidbody;
+
+    bool isOnGround;
+    public GameObject groundChecker;
+    public LayerMask groundLayer;
+    public float jumpForce = 300.0f;
+
     float rotationSpeed = 2.0f;
     float camRotationSpeed = 1.5f;
     
@@ -21,8 +31,14 @@ public class CharacterController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-        Vector3 newVelocity = transform.forward * Input.GetAxis("Vertical") * maxSpeed;
+        isOnGround = Physics.CheckSphere(groundChecker.transform.position, 0.1f, groundLayer);
+
+        if (isOnGround == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidbody.AddForce(transform.up * jumpForce);
+        }
+
+        Vector3 newVelocity = (transform.forward * Input.GetAxis("Vertical") * maxSpeed) + (transform.right * Input.GetAxis("Horizontal") * maxSpeed);
         myRigidbody.velocity = new Vector3(newVelocity.x, myRigidbody.velocity.y, newVelocity.z);
         rotation = rotation + Input.GetAxis("Mouse X") * rotationSpeed;
         transform.rotation = Quaternion.Euler(new Vector3(0.0f, rotation, 0.0f));
